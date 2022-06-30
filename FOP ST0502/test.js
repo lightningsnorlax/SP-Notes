75a6b9f482c1d4fb58603e35e4566fa435d35e1f
lightningsnorlax <ianlimyuyang@outlook.com>
2022-06-30 10:13:29 +0800

    hi

--- FOP ST0502/test.js
+++ FOP ST0502/test.js
@@ -1,8 +1,8 @@
-x = true;
-if (x) {
-    const y = "hello";
-    console.log(y);
-} else {
-    const y = "byebye";
+function hi() {
+    var x = 5;
+    x = 1243567 + 12345678;
+    return x;
 }
-console.log(y);
+
+var bye = hi()
+console.log(bye)
